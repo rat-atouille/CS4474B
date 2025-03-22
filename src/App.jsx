935cfb9e271db002ce51e9087d800979ce0416c9
arjunsharma <ashar562@uwo.ca>
2025-03-22 18:01:28 -0400

    Fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter } from 'react-router'
+import {BrowserRouter, Routes} from 'react-router'
 import './App.css'
 
 
 // make sure to route urs here 
 // <Routepath="/pathName" element={<className />} />
 function App() {
-    return (
-      <>
-      <BrowserRouter>
-        <Routes>
-        </Routes>
-      </BrowserRouter>
-      </>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
