d06357115e95259b2e508d8959f02b9b9081e5bf
arjunsharma <ashar562@uwo.ca>
2025-03-25 16:58:53 -0400

    Re-add layout

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import Layout from "./components/layout/Layout.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-          <App/>
+    <Layout>
+      <App/>
+    </Layout>
   </StrictMode>,
 )
