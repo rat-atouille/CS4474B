032a7d916a5b919ebff6955d72256fd9ee5c9e94
ka1en <122689477+ka1en@users.noreply.github.com>
2025-03-26 22:10:59 -0400

    Added Browse content

---
+++ src/assets/Browse/constants.js
@@ -0,0 +1,472 @@
+// Music
+import afroCover from './afroCover.png'
+import alternativeCover from './alternativeCover.png'
+import arabCover from './arabCover.png'
+import blueCover from './blueCover.png'
+import caribbeanCover from './caribbeanCover.png'
+import classicalCover from './classicalCover.png'
+import countryCover from './countryCover.png'
+import danceCover from './danceCover.png'
+import desiCover from './desiCover.png'
+import folkCover from './folkCover.png'
+import hiphopCover from './hiphopCover.png'
+import indieCover from './indieCover.png'
+import latinCover from './latinCover.png'
+import popCover from './popCover.png'
+import punkCover from './punkCover.png'
+import rockCover from './rockCover.png'
+import salsaCover from './salsaCover.png'
+import soulCover from './soulCover.png'
+import rnbCover from './rnbCover.png'
+import jazzCover from './jazzCover.png'
+// Icon
+import homeIcon from './homeIcon.png'
+import searchIcon from './searchIcon.png'
+import bookIcon from './bookIcon.png'
+import plusIcon from './plusIcon.png'
+import arrowIcon from './arrowIcon.png'
+import backArrowIcon from './backArrowIcon.png'
+import forwardArrowIcon from './forwardArrowIcon.png'
+// Podcast
+import artCover from './artCover.png'
+import beautyCover from './beautyCover.png'
+import bookCover from './bookCover.png'
+import businessCover from './businessCover.png'
+import comedyCover from './comedyCover.png'
+import crimeCover from './crimeCover.png'
+import designCover from './designCover.png'
+import documentaryCover from './documentaryCover.png'
+import educationalCover from './educationalCover.png'
+import filmCover from './filmCover.png' 
+import fictionCover from './fictionCover.png'
+import financeCover from './financeCover.png'
+import fitnessCover from './fitnessCover.png'
+import gamesCover from './gamesCover.png' 
+import historyCover from './historyCover.png'
+import newsCover from './newsCover.png'
+import popCultureCover from './popCultureCover.png'
+import scienceCover from './scienceCover.png'
+import techCover from './techCover.png'
+import topPodcastCover from './topPodcastCover.png'
+
+export const assets = {
+  afroCover,
+  alternativeCover,
+  arabCover,
+  blueCover,
+  caribbeanCover,
+  classicalCover,
+  countryCover,
+  danceCover,
+  desiCover,
+  folkCover,
+  hiphopCover,
+  indieCover,
+  latinCover,
+  popCover,
+  punkCover,
+  rockCover,
+  rnbCover,
+  salsaCover,
+  soulCover,
+  jazzCover,
+  // Icon
+  homeIcon,
+  searchIcon,
+  topPodcastCover,
+  bookIcon,
+  plusIcon,
+  arrowIcon,
+  backArrowIcon,
+  forwardArrowIcon,
+  // Pocast
+  artCover,
+  beautyCover,
+  bookCover,
+  businessCover,
+  comedyCover,
+  crimeCover,
+  designCover,
+  documentaryCover,
+  educationalCover,
+  fictionCover,
+  filmCover,
+  financeCover,
+  fitnessCover,
+  gamesCover,
+  historyCover,
+  newsCover,
+  popCultureCover,
+  scienceCover,
+  techCover,
+  topPodcastCover,
+}
+
+export const songData = [
+  {
+    id:0,
+    name: "Weekly Afro Hit",
+    genre: "Afro",
+    image: afroCover
+  },
+  {
+    id:1,
+    name: "Weekly Alternative Hit",
+    genre: "Alternative",
+    image: alternativeCover
+  },
+  {
+    id:2,
+    name: "Weekly Arab Hit",
+    genre: "Arab",
+    image: arabCover
+  },
+  {
+    id:3,
+    name: "Weekly Blue Hit",
+    genre: "Blue",
+    image: blueCover
+  },
+  {
+    id:4,
+    name: "Weekly Caribbean Hit",
+    genre: "Caribbean",
+    image: caribbeanCover
+  },
+  {
+    id:5,
+    name: "Weekly Classical Hit",
+    genre: "Classical",
+    image: classicalCover
+  },
+  {
+    id:6,
+    name: "Weekly Country Hit",
+    genre: "Country",
+    image: countryCover
+  },
+  {
+    id:7,
+    name: "Weekly Dance Hit",
+    genre: "Dance",    
+    image: danceCover
+  },
+  {
+    id:8,
+    name: "Weekly Desi Hit",
+    genre: "Desi",
+    image: desiCover
+  },
+  {
+    id:9,
+    name: "Weekly Folk Hit",
+    genre: "Folk",
+    image: folkCover
+  },
+  {
+    id:10,
+    name: "Weekly Hip Hop Hit",
+    genre: "Hip Hop",
+    image: hiphopCover
+  },
+  {
+    id:11,
+    name: "Weekly Indie Hit",
+    genre: "Indie",
+    image: indieCover
+  },
+  {
+    id:12,
+    name: "Weekly Latin Hit",
+    genre: "Latin",
+    image: latinCover
+  },
+  {
+    id:13,
+    name: "Weekly Pop Hit",
+    genre: "Pop",
+    image: popCover
+  },
+  {
+    id:14,
+    name: "Weekly Punk Hit",
+    genre: "Punk",
+    image: punkCover
+  },
+  {
+    id:15,
+    name: "Weekly Rock Hit",
+    genre: "Rock",
+    image: rockCover
+  },
+  {
+    id:16,
+    name: "Weekly R&B Hit",
+    genre: "R&B",
+    image: rnbCover
+  },
+  {
+    id:17,
+    name: "Weekly Salsa Hit",
+    genre: "Salsa",
+    image: salsaCover
+  },
+  {
+    id:18,
+    name: "Weekly Soul Hit",
+    genre: "Soul",
+    image: soulCover
+  },
+  {
+    id:19,
+    name: "Weekly Jazz Hit",
+    genre: "Jazz",
+    image: jazzCover
+  },
+]
+
+export const podcastData = [
+  {
+    id:20,
+    name: "Art Podcast",
+    genre: "Art",
+    image: artCover
+  },
+  {
+    id:21,
+    name: "Beauty Podcast",
+    genre: "Beauty",
+    image:  beautyCover
+  },
+  {
+    id:22,
+    name: "Book Podcast",
+    genre: "Book",
+    image: bookCover
+  }, 
+  {
+    id:23,
+    name: "Business Podcast",
+    genre: "Business",
+    image: businessCover
+  }, 
+  {
+    id:24,
+    name: "Comedy Podcast",
+    genre: "Comedy",
+    image: comedyCover
+  },
+  {
+    id:25,
+    name: "Crime Podcast",
+    genre: "Crime",
+    image: crimeCover
+  },
+  {
+    id:26,
+    name: "Design Podcast",
+    genre: "Design",
+    image: designCover
+  },
+  {
+    id:27,
+    name: "Documentary Podcast",
+    genre: "Documentary",
+    image: documentaryCover
+  },
+  {
+    id:28,
+    name: "Educational Podcast",
+    genre: "Educational",
+    image:  educationalCover
+  },
+  {
+    id:29,
+    name: "Fiction Podcast",
+    genre: "Fiction",
+    image: fictionCover
+  },
+  {
+    id:30,
+    name: "Film Podcast",
+    genre: "Film",
+    image: filmCover
+  },
+  {
+    id:31,
+    name: "Finance Podcast",
+    genre: "Finance",
+    image: financeCover
+  },
+  {
+    id:32,
+    name: "Fitness Podcast",
+    genre: "Fitness",
+    image: fitnessCover
+  },
+  {
+    id:33,
+    name: "Games Podcast",
+    genre: "Games",
+    image: gamesCover
+  },
+  {
+    id:34,
+    name: "History Podcast",
+    genre: "History",
+    image: historyCover
+  },
+  {
+    id:35,
+    name: "News Podcast",
+    genre: "News",
+    image: newsCover
+  },
+  {
+    id:36,
+    name: "Pop Culture Podcast",
+    genre: "Pop Culture",
+    image:  popCultureCover
+  }, 
+  {
+    id:37,
+    name: "Science Podcast",
+    genre: "Scienc",
+    image:  scienceCover
+  },
+  {
+    id:38,
+    name: "Tech Podcast",
+    genre: "Tech",
+    image:  techCover
+  },
+  {
+    id:39,
+    name: "Top Podcast",
+    genre: "Top",
+    image:  topPodcastCover
+  },
+]
+
+export const audiobookData = [
+  {
+    id:0,
+    name: "Weekly Afro Hit",
+    genre: "Afro",
+    image: afroCover
+  },
+  {
+    id:1,
+    name: "Weekly Alternative Hit",
+    genre: "Alternative",
+    image: alternativeCover
+  },
+  {
+    id:2,
+    name: "Weekly Arab Hit",
+    genre: "Arab",
+    image: arabCover
+  },
+  {
+    id:3,
+    name: "Weekly Blue Hit",
+    genre: "Blue",
+    image: blueCover
+  },
+  {
+    id:4,
+    name: "Weekly Caribbean Hit",
+    genre: "Caribbean",
+    image: caribbeanCover
+  },
+  {
+    id:5,
+    name: "Weekly Classical Hit",
+    genre: "Classical",
+    image: classicalCover
+  },
+  {
+    id:6,
+    name: "Weekly Country Hit",
+    genre: "Country",
+    image: countryCover
+  },
+  {
+    id:7,
+    name: "Weekly Dance Hit",
+    genre: "Dance",    
+    image: danceCover
+  },
+  {
+    id:8,
+    name: "Weekly Desi Hit",
+    genre: "Desi",
+    image: desiCover
+  },
+  {
+    id:9,
+    name: "Weekly Folk Hit",
+    genre: "Folk",
+    image: folkCover
+  },
+  {
+    id:10,
+    name: "Weekly Hip Hop Hit",
+    genre: "Hip Hop",
+    image: hiphopCover
+  },
+  {
+    id:11,
+    name: "Weekly Indie Hit",
+    genre: "Indie",
+    image: indieCover
+  },
+  {
+    id:12,
+    name: "Weekly Latin Hit",
+    genre: "Latin",
+    image: latinCover
+  },
+  {
+    id:13,
+    name: "Weekly Pop Hit",
+    genre: "Pop",
+    image: popCover
+  },
+  {
+    id:14,
+    name: "Weekly Punk Hit",
+    genre: "Punk",
+    image: punkCover
+  },
+  {
+    id:15,
+    name: "Weekly Rock Hit",
+    genre: "Rock",
+    image: rockCover
+  },
+  {
+    id:16,
+    name: "Weekly R&B Hit",
+    genre: "R&B",
+    image: rnbCover
+  },
+  {
+    id:17,
+    name: "Weekly Salsa Hit",
+    genre: "Salsa",
+    image: salsaCover
+  },
+  {
+    id:18,
+    name: "Weekly Soul Hit",
+    genre: "Soul",
+    image: soulCover
+  },
+  {
+    id:19,
+    name: "Weekly Jazz Hit",
+    genre: "Jazz",
+    image: jazzCover
+  },
+]
