4e7e7566e9137401e7117ac3ab63ac77bfce2793
Fahmid-Abdullah <fahmidabdullah@hotmail.com>
2025-03-26 21:08:02 -0400

    navbar v2

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,26 +1,33 @@
-function Navbar({showNavBackground}) {
+function Navbar({ showNavBackground }) {
   return (
     <div
-      className={`bg-black w-full h-[5vw] z-10 px-4 py-2 flex items-center justify-between transition-all ${showNavBackground && "bg-[#1a1a1a]"}`}>
+      className={`bg-black w-full h-[10vh] md:h-[5vw] z-10 px-4 py-2 flex items-center justify-between transition-all ${
+        showNavBackground && "bg-[#1a1a1a]"
+      }`}
+    >
       {/* Left Icon and Search Bar in Center */}
       <div className="flex-1 flex justify-center items-center space-x-4">
-        <i
-          className="fa-solid fa-house text-2xl hover:text-white hover:scale-110 transition-all duration-150 ease-in-out"></i>
-        <input
-          type="text"
-          placeholder="What do you want to play?"
-          className="w-xl text-lg pl-3 pr-4 py-2 rounded-lg bg-stone-600 text-white focus:outline-none focus:ring-2 focus:ring-stone-500"
-        />
+        <i className="fa-solid fa-house text-lg md:text-xl hover:text-white hover:scale-110 transition-all duration-150 ease-in-out"></i>
+        <div className="relative flex items-center rounded-full py-1 text-white bg-stone-800 hover:bg-stone-600 focus-within:border-2 focus-within:bg-stone-700 transition-all duration-150 ease-in-out px-4 w-full max-w-xs sm:max-w-md md:max-w-lg">
+          <i className="fa-solid fa-search text-lg md:text-xl hover:text-white hover:scale-110 transition-all duration-150 ease-in-out"></i>
+          <input
+            type="text"
+            placeholder="What do you want to play?"
+            className="w-full text-sm md:text-md pl-3 pr-4 outline-none bg-transparent"
+          />
+          {/* Vertical Divider */}
+          <div className="h-6 w-px bg-gray-400 mx-3 hidden sm:block"></div>
+          <i className="fa-solid fa-box-archive text-lg md:text-xl hover:text-white hover:scale-110 transition-all duration-150 ease-in-out hidden sm:block"></i>
+        </div>
       </div>
 
       {/* Right Icons */}
-      <div className="flex items-center space-x-4 mr-8">
-        <i
-          className="fa-solid fa-bell text-3xl hover:text-white hover:scale-110 transition-all duration-150 ease-in-out"></i>
+      <div className="flex items-center space-x-3 md:space-x-4 mr-4 md:mr-8">
+        <i className="fa-solid fa-bell text-lg md:text-xl hover:text-white hover:scale-110 transition-all duration-150 ease-in-out"></i>
         <img
-          src="src\assets\profileIcon.png"
+          src="src/assets/profileIcon.png"
           alt="Thumbnail"
-          className="h-12 w-12 object-cover rounded-full hover:text-white"
+          className="h-6 w-6 md:h-8 md:w-8 object-cover rounded-full hover:text-white"
         />
       </div>
     </div>
