740a563b81ebcfde5d7832fcefec73dbe98567f5
ka1en <karenbotchway2@gmail.com>
2025-03-27 18:37:11 -0400

    Browse colour scheme matches homepage's

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -25,13 +25,12 @@ const BrowsePage = () => {
   const dataToDisplay = getData();
 
     return (
-      <div>
-        <div className="flex items-center gap-2 mt-25 ml-4">
-          <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center">K</p>
-          <p className={`px-4 py-1 rounded-2xl cursor-pointer ${activeFilter === "All" ? "bg-white text-black" : "bg-black text-white"}`} onClick={()=>handleClick("All")}>All</p>
-          <p className={`px-4 py-1 rounded-2xl cursor-pointer ${activeFilter === "Music" ? "bg-white text-black" : "bg-black text-white"}`} onClick={()=>handleClick("Music")}>Music</p>
-          <p className={`px-4 py-1 rounded-2xl cursor-pointer ${activeFilter === "Podcast" ? "bg-white text-black" : "bg-black text-white"}`} onClick={()=>handleClick("Podcast")}>Podcast</p>
-          <p className={`px-4 py-1 rounded-2xl cursor-pointer ${activeFilter === "Audiobooks" ? "bg-white text-black" : "bg-black text-white"}`} onClick={()=>handleClick("Audiobooks")}>Audiobooks</p>
+      <div className="bg-[#212121] min-h-screen flex flex-col">
+        <div className="flex items-center gap-2 mt-7 ml-4">
+          <p className={`px-4 py-1 rounded-2xl font-semibold cursor-pointer ${activeFilter === "All" ? "bg-green-500 text-white" : "bg-gray-700 text-gray-300"}`} onClick={()=>handleClick("All")}>All</p>
+          <p className={`px-4 py-1 rounded-2xl font-semibold cursor-pointer ${activeFilter === "Music" ? "bg-green-500 text-white" : "bg-gray-700 text-gray-300"}`} onClick={()=>handleClick("Music")}>Music</p>
+          <p className={`px-4 py-1 rounded-2xl  font-semibold cursor-pointer ${activeFilter === "Podcast" ? "bg-green-500 text-white" : "bg-gray-700 text-gray-300"}`} onClick={()=>handleClick("Podcast")}>Podcast</p>
+          <p className={`px-4 py-1 rounded-2xl font-semibold cursor-pointer ${activeFilter === "Audiobooks" ? "bg-green-500 text-white" : "bg-gray-700 text-gray-300"}`} onClick={()=>handleClick("Audiobooks")}>Audiobooks</p>
         </div>
         <div className="mb-4">
           <h1 className="ml-4 my-5 font-bold text-2xl">Genres</h1>
