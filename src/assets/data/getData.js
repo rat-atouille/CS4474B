7e75eb1bc776556b6f033546d1730504429a2768
arjunsharma <ashar562@uwo.ca>
2025-03-29 17:49:07 -0400

    finish and create files for getting data
    JSON files contain the data, js files contain how we get the data. You can modify how much data you get. For the general data, you can modify the search parameter.

--- src/assets/data/getData.js
+++ src/assets/data/getData.js
@@ -1,5 +1,21 @@
-// This file was used to get the data we needed for our redesign.
+import {writeFileSync} from 'fs';
 import axios from 'axios';
+
+// RUNNING WILL OVERWRITE THE CURRENT "data.json" FILE, DO NOT RUN IF YOU NEED CURRENT data.json FILE!!!
+
+// Edit these limits to specify the maximum amount of "items" you want. Max limit is 50;
+// BE CAREFUL: albums are pulled from artists, songs pulled from albums. So for 1 artist, you'll get
+// ALBUMS_LIMIT number of albums, and for 1 album, you'll get SONGS_LIMIT number of songs. exponential growth!
+// API key is not infinite so keep that in mind. Recommended: ARTISTS > ALBUMS
+const ARTISTS_LIMIT = "5";
+const ALBUMS_LIMIT = "5";
+const SONGS_LIMIT = "5";
+
+// The search query that we use to get the ARTISTS. Details found here: https://developer.spotify.com/documentation/web-api/reference/search
+const QUERY = "all"
+
+
+const finalJson = {};
 
 const CLIENT_ID = "1500e4eb90b34eecb403201c86d3611e";
 const CLIENT_SECRET = "ea2e38c9737a43e7a6735c17315b7e7d";
@@ -22,33 +38,66 @@ async function getAccessToken() {
   return response.data.access_token;
 }
 
-export async function getArtistData(access_token) {
+async function getArtistData(access_token) {
   let url = new URL("https://api.spotify.com/v1/search");
-  url.searchParams.append("q", "genre:pop");
+  url.searchParams.append("q", QUERY);
   url.searchParams.append("type", "artist");
-  url.searchParams.append("limit", "2")
-  let options = {
+  url.searchParams.append("limit", ARTISTS_LIMIT);
+
+  const options = {
     headers: {
       'Authorization': `Bearer ${access_token}`
     }
   }
+  // ----- GETTING ARTIST -------------
+  let {data: {artists: {items}}} = await axios.get(url.toString(), options);
+  // --------------------------------------------
 
-  let response = await axios.get(url.toString(), options);
-  if (response.status !== 200) {
-    throw new Error(response.statusText);
-  }
-  const items = response.data.artists.items;
-  items.forEach(async (artist) => {
+  // map resolves each individual promise, promise.all collects all promises resolved
+  await Promise.all(items.map(async (artist) => {
+
+    // ----- GETTING ALBUMS FROM ARTIST -------------
     url = new URL(`https://api.spotify.com/v1/artists/${artist.id}/albums`);
-    url.searchParams.append("limit", "2");
-    response = await axios.get(url.toString(), options);
-    console.log(response.data)
-  })
+    url.searchParams.append("limit", ALBUMS_LIMIT);
+    const {data: {items: albums}} = await axios.get(url.toString(), options);
+    // --------------------------------------------
+
+    finalJson[artist.name] = {
+      followers: artist.followers.total,
+      genres: artist.genres,
+      image: artist.images[0].url,
+      albums: albums.map(album => ({
+        name: album.name, image: album.images[0].url, releaseDate: album.release_date,
+        songs: []
+      }))
+    }
+
+    // ------ GETTING SONGS FROM ALBUMS -----------
+    await Promise.all(albums.map(async ({id, name}) => {
+      let url = new URL(`https://api.spotify.com/v1/albums/${id}/tracks`)
+      url.searchParams.append("limit", SONGS_LIMIT);
+      const {data: {items: songs}} = await axios.get(url.toString(), options);
+
+
+      // find where album should be
+      const foundAlbum = finalJson[artist.name].albums.find(album => album.name === name);
+      foundAlbum.songs = songs.map((song) => ({
+        name: song.name,
+        durationMs: song.duration_ms,
+        image: foundAlbum.image
+      }));
+      // --------------------------------------------
+    }))
+  }))
 }
+
 
 (async () => {
   const access_token = await getAccessToken();
   await getArtistData(access_token);
+
+  console.log(finalJson)
+  writeFileSync("data.json", JSON.stringify(finalJson));
 })();
 
 
