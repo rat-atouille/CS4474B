fd0438598fcad261b73f45e7cfa1292e6e089021
arjunsharma <ashar562@uwo.ca>
2025-04-02 16:22:08 -0400

    Fix up album page
    Make layout a table
    Linked artist page from artist name

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { useAlbum } from '../context/AlbumContext';
-import { IoPlay, IoPause, IoHeart, IoHeartOutline } from "react-icons/io5";
-import { BsSoundwave } from "react-icons/bs";
+import React, {useEffect, useState} from 'react';
+import {useAlbum} from '../context/AlbumContext';
+import {IoPlay, IoPause, IoHeart, IoHeartOutline} from "react-icons/io5";
+import {BsSoundwave} from "react-icons/bs";
 
-export default function Album({ setMusicQueue }) {
+export default function Album({setMusicQueue}) {
   const [hoveredIndex, setHoveredIndex] = useState(null);
   const [playIndex, setPlayIndex] = useState(null);
   const [currentAlbum] = useAlbum();
   const [likedSongs, setLikedSongs] = useState([]);
 
-  if (!currentAlbum) return <div className="flex justify-center items-center h-screen text-5xl text-red-500 font-extrabold tracking-widest">ERROR 404</div>;
+  if (!currentAlbum) return <div
+    className="flex justify-center items-center h-screen text-5xl text-red-500 font-extrabold tracking-widest">ERROR
+    404</div>;
 
   // Helper function to convert milliseconds to MM:SS format
   const convertToMMSS = (durationMs) => {
@@ -26,11 +28,11 @@ export default function Album({ setMusicQueue }) {
 
   const handlePlay = (album, index) => {
     if (typeof setMusicQueue === 'function') {
-      setMusicQueue({ ...album, index });
+      setMusicQueue({...album, index});
     } else {
       console.error('setMusicQueue is not a function');
     }
-  };  
+  };
 
   const togglePlay = (e, index) => {
     e.stopPropagation();
@@ -40,7 +42,7 @@ export default function Album({ setMusicQueue }) {
       setPlayIndex(index);
     }
   };
-  
+
   const toggleLike = (e, songId) => {
     e.stopPropagation(); // Prevent triggering the parent div's onClick
     if (likedSongs.includes(songId)) {
@@ -54,67 +56,79 @@ export default function Album({ setMusicQueue }) {
   const renderTabContent = () => (
     <div>
       <div className="flex items-center p-0 mb-4 mt-2 text-2xl text-gray-300 space-x-4">
-        <i className="fa-solid fa-circle-play text-5xl text-green-500 hover:text-green-400 hover:scale-105 transition-all duration-300 ease-in-out"
-        onClick={() => handlePlay(currentAlbum)}></i>
+        <i
+          className="fa-solid fa-circle-play text-5xl text-green-500 hover:text-green-400 hover:scale-105 transition-all duration-300 ease-in-out"
+          onClick={() => handlePlay(currentAlbum)}></i>
         <i className="fa-solid fa-shuffle hover:text-white transition-all duration-300 ease-in-out"></i>
-        <i className="fa-solid fa-plus text-sm border-3 p-1 rounded-full hover:text-white transition-all duration-300 ease-in-out"></i>
-        <i className="fa-solid fa-arrow-down text-sm border-3 py-1 px-1.5 rounded-full hover:text-white transition-all duration-300 ease-in-out"></i>
+        <i
+          className="fa-solid fa-plus text-sm border-3 p-1 rounded-full hover:text-white transition-all duration-300 ease-in-out"></i>
+        <i
+          className="fa-solid fa-arrow-down text-sm border-3 py-1 px-1.5 rounded-full hover:text-white transition-all duration-300 ease-in-out"></i>
         <i className="text-sm hover:text-white transition-all duration-300 ease-in-out">•••</i>
       </div>
       <div className="rounded">
-        <div className="rounded-lg p-3 mb-2 border-b border-gray-700">
-          <div className="flex justify-between ml-3 text-gray-400">
-            <div className="flex space-x-5">
-              <p>#</p>
-              <p>Title</p>
-            </div>
-            <div className="hidden md:block ml-66">Plays</div>
-            <div className="mr-8"><i className="fa-regular fa-clock"></i></div>
-          </div>
-        </div>
+        <table className="w-full text-sm text-left text-gray-400">
+          <thead>
+          <tr className="p-3 mb-2 border-b border-gray-700">
+            <th className="p-3 pl-6">#</th>
+            <th className="p-3">Title</th>
+            <th className="hidden md:table-cell p-3">Plays</th>
+            <th className="p-3 text-center"><i className="fa-regular fa-clock"></i></th>
+            <th className="p-3 text-center"></th>
+          </tr>
+          </thead>
+          <tbody>
+          {currentAlbum.album.songs && currentAlbum.album.songs.map((song, index) => (
+            <tr
+              key={index}
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
+              onClick={(e) => {
+                handlePlay(currentAlbum, index);
+                togglePlay(e, index);
+              }}
+              className="hover:bg-[#535353] hover:cursor-pointer border-b border-gray-700">
+              {/* Index / Play Icon / Soundwave */}
+              <td className="w-8 text-center text-xs text-gray-400">
+                {playIndex === index ? (
+                  <BsSoundwave className="text-green-500 text-lg ml-2"/>
+                ) : hoveredIndex === index ? (
+                  <IoPlay className="text-lg ml-2"/>
+                ) : (
+                  index + 1
+                )}
+              </td>
 
-        {/* Map over songs */}
-        {currentAlbum.album.songs && currentAlbum.album.songs.map((song, index) => (
-          <div
-            key={index}
-            onMouseEnter={() => setHoveredIndex(index)}
-            onMouseLeave={() => setHoveredIndex(null)}
-            onClick={(e) => togglePlay(e, index)}
-            className="rounded-lg flex items-center p-3 border-b border-gray-700 hover:cursor-pointer hover:bg-[#535353] relative"
-          >
-            <div
-              className="w-8 text-center text-gray-400 text-xs mr-3"
-              onClick={() => {handlePlay(currentAlbum, index);
-                (e) => togglePlay(e, index);}}
-            >
-              {playIndex === index ? 
-                <BsSoundwave className="text-green-500 text-lg ml-2" /> : 
-                (hoveredIndex === index ? <IoPlay className="text-lg ml-2" /> : index + 1)
-              }
-            </div>
+              {/* Title and Artist */}
+              <td className="text-white py-3 pr-3">
+                <div>
+                  <p className="text-white text-sm w-3/5 md:w-xs">{song.name}</p>
+                  <a href={`/artist/?name=${currentAlbum.artist}`} className="text-gray-400 text-sm hover:underline transition-all duration-300 ease-in-out">
+                    {currentAlbum.artist}
+                  </a>
+                </div>
+              </td>
 
-            <div className="relative flex justify-between items-center w-full">
-              <div>
-                <p className="text-white text-sm w-3/5 md:w-xs">{song.name}</p>
-                <p className="text-gray-400 text-sm hover:underline transition-all duration-300 ease-in-out">{currentAlbum.artist}</p>
-              </div>
-              <p className="hidden md:block text-sm">1,000,000</p>
-              <div className="text-gray-400 mr-7 text-sm">{convertToMMSS(song.durationMs)}</div>
+              {/* Plays */}
+              <td className="hidden md:table-cell text-sm">1,000,000</td>
 
-              {/* Liked song */}
-              <div 
-                className="text-gray-400 mr-8 cursor-pointer"
-                onClick={(e) => toggleLike(e, song.id)}
-              >
-                {likedSongs.includes(song.id) ? 
-                  <IoHeart className="text-red-500" /> : 
-                  (hoveredIndex === `recent-${index}` ? <IoHeartOutline /> : null)
-                }
-              </div>
-            </div>
-          </div>
-        ))}
+              {/* Duration */}
+              <td className="text-gray-400 text-sm text-center">{convertToMMSS(song.durationMs)}</td>
+
+              {/* Like Icon */}
+              <td className="text-gray-400 text-center cursor-pointer" onClick={(e) => toggleLike(e, song.id)}>
+                {likedSongs.includes(song.id) ? (
+                  <IoHeart className="text-red-500"/>
+                ) : (
+                  hoveredIndex === `recent-${index}` ? <IoHeartOutline/> : null
+                )}
+              </td>
+            </tr>
+          ))}
+          </tbody>
+        </table>
       </div>
+
 
       <div className="ml-3 mr-3">
         <h2 className="text-xl font-bold mb-4 mt-10">More by {currentAlbum.artist}</h2>
@@ -123,13 +137,15 @@ export default function Album({ setMusicQueue }) {
           {currentAlbum.album.songs.map((album, index) => (
             <div key={index} className="p-3 rounded group">
               <div className="relative">
-                <img 
-                  src={album.image} 
-                  alt="Song Image" 
+                <img
+                  src={album.image}
+                  alt="Song Image"
                   className="object-cover rounded"
                 />
-                <button className="absolute bottom-2 right-2 opacity-0 bg-black rounded-full group-hover:opacity-100 transition-all ease-in-out">
-                  <i className="fa-solid fa-circle-play text-5xl text-green-500 hover:scale-105 transition-all duration-150 ease-in-out"></i>
+                <button
+                  className="absolute bottom-2 right-2 opacity-0 bg-black rounded-full group-hover:opacity-100 transition-all ease-in-out">
+                  <i
+                    className="fa-solid fa-circle-play text-5xl text-green-500 hover:scale-105 transition-all duration-150 ease-in-out"></i>
                 </button>
               </div>
               <p className="text-white mt-2 truncate font-semibold text-sm">{album.name}</p>
@@ -144,17 +160,19 @@ export default function Album({ setMusicQueue }) {
   return (
     <div className="bg-[#212121] text-white w-full">
       <div className="relative h-64 bg-red-900 p-6 flex items-end space-x-4">
-        <img 
+        <img
           src={currentAlbum == null ? "/placeHolders/placeHolderIcon.jpeg" : currentAlbum.album.image}
-          alt="Album Image" 
+          alt="Album Image"
           className="object-cover rounded w-1/3 md:w-1/4 xl:w-1/6 z-10"
         />
         <div className="relative z-10">
           <span className="text-xs px-2 py-1 rounded-sm mb-1 inline-block select-none">Album</span>
-          <h1 className="text-xl sm:text-sm md:text-3xl lg:text-4xl xl:text-5xl w-full font-bold">{currentAlbum.album.name}</h1>
-          <p className="text-xs mt-2 text-[#b3b3b3] select-none">
-            {currentAlbum.artist} • {currentAlbum.album.releaseDate.substring(0, 4)} • {currentAlbum.album.songs.length} songs • {totalDuration(currentAlbum.album.songs)}
-          </p>
+          <h1
+            className="text-xl sm:text-sm md:text-3xl lg:text-4xl xl:text-5xl w-full font-bold">{currentAlbum.album.name}</h1>
+          <a href={`/album/?name=${currentAlbum.artist}`} className="text-xs mt-2 text-[#b3b3b3] select-none">
+            {currentAlbum.artist} • {currentAlbum.album.releaseDate.substring(0, 4)} • {currentAlbum.album.songs.length} songs
+            • {totalDuration(currentAlbum.album.songs)}
+          </a>
         </div>
         <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent opacity-80"></div>
       </div>
