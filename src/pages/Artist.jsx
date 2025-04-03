bd939a4bc6a5b19190eb561e01d3af864f4ae524
rat-atouille <heenap0113@gmail.com>
2025-04-03 17:02:04 -0400

    fixed artist

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -458,12 +458,12 @@ const Artist = ({setMusicQueue, currentSong}) => {
             <h2 className="text-xl font-bold mb-4">Albums</h2>
             <div className=" mx-5 p-0 m-0 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
                 {albums.map(album => (
-                  <Link to={`/album/?name=${album.title}&type=album`} key={album.id} className="p-3 m-0 rounded hover:bg-gray-700 transition-all">
+                  <div  key={album.id} className="p-3 m-0 rounded hover:bg-gray-700 transition-all">
                     {/* Square Album Cover */}
                     <div key={album.id}>
                     <div className="w-full relative aspect-square mb-2 rounded overflow-hidden">
                       <img src={album.image} alt={album.title} className="w-full h-full object-cover" />
-                      <button className="absolute right-3 top-3 cursor-pointer"
+                      <button className="absolute right-3 top-3 cursor-pointer z-30"
                       onClick={(e) => toggleLikeAlbum(e, album.id)}
                     >
                       {likedAlbums.includes(album.id) ?
@@ -477,7 +477,7 @@ const Artist = ({setMusicQueue, currentSong}) => {
                     <div />
                     <Link to={`/album/?name=${album.title}&type=album`} href={`/album/?name=${album.title}`} className="select-none text-gray-400 text-sm">{album.year} • {album.tracks} tracks</Link>
                     </div>
-                  </Link>
+                  </div>
                 ))}
               </div>
           </div>
@@ -496,8 +496,7 @@ const Artist = ({setMusicQueue, currentSong}) => {
               )}
               {/* Display Singles & EPs */}
               {albums.filter(album => album.tracks <= 3).map(album => (
-                 <Link to={`/album/?name=${album.title}&type=album`} key={album.id} className="p-3 m-0 rounded hover:bg-gray-700 transition-all">
-                <div key={album.id} className=" hover:bg-gray-700  p-4 rounded">
+                <div key={album.id} className=" hover:bg-gray-700  p-3 m-0 rounded hover:bg-gray-700 transition-all">
                   {/* Album Cover */}
                   <div className="w-full relative aspect-square mb-2 rounded overflow-hidden">
                   <img src={album.image} alt={album.title} className="w-full h-full object-cover" />
@@ -516,10 +515,11 @@ const Artist = ({setMusicQueue, currentSong}) => {
                   </div>
 
                   {/* Album Info */}
+                  <Link to={`/album/?name=${album.title}&type=album`}>
                   <p className="text-white font-bold">{album.title}</p>
                   <p className="text-gray-400 text-sm">Singles • {album.year}</p>
+                  </Link>
                 </div>
-                </Link>
               ))}
             </div>
 
