2367c7291fd737a6c5439b436e839a541159b59a
rat-atouille <heenap0113@gmail.com>
2025-04-03 17:09:09 -0400

    fixed artist

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -317,7 +317,7 @@ const Artist = ({setMusicQueue, currentSong}) => {
             </div>
 
             <div className="">
-                <h2 className="text-xl font-bold mb-4 mt-10">Recommended for You</h2>
+                <h2 className="select-none text-xl font-bold mb-4 mt4">Recommended for You</h2>
                 <div className=" mx-5 p-0 m-0 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
                 {albums.map(album => (
                   <Link to={`/album/?name=${album.title}&type=album`} key={album.id} className="p-3 m-0 rounded hover:bg-gray-700 transition-all">
