5ba5d024765d9da0e57b330eb9401719a7d4e907
Fahmid-Abdullah <fahmidabdullah@hotmail.com>
2025-04-03 16:02:40 -0400

    shuffle array

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -10,6 +10,15 @@ export default function SearchPage({ setMusicQueue }) {
     const [searchParam, setSearchParam] = useState("");
     const [activeCategory, setActiveCategory] = useState("All");
 
+    function shuffleArray(array) {
+        const newArray = [...array];
+        for (let i = newArray.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+        }
+        return newArray;
+      }
+
     useEffect(() => {
         const queryString = new URLSearchParams(location.search);
         const query = queryString.get("q") || "";
